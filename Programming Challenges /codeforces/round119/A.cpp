fb26c37ee07fa89b6296174dbd58da922143d2b5
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2018-09-28 12:36:10 -0500

    Adding some codeforces solved problems

--- Programming Challenges /codeforces/round119/A.cpp
+++ Programming Challenges /codeforces/round119/A.cpp
@@ -1,7 +1,45 @@
 #include <cstdio>
 using namespace std;
+//In this method its guaranteed that a <= b <= c
+int process(int n, int a, int b, int c, int i) {
+  if (n < a || n <= 0) return i;
+  else {
+    int moda, modb, modc;
+    moda = n % a;
+    modb = n % b;
+    modc = n % c;
+    if(moda > modb && moda > modc && n / a > 0) {
+      return process(n - n / a, a, b, c, i + 1);
+    } else if(modb > moda && modb > modc && n / b > 0) {
+      return process(n - (n / b), a, b, c, i + 1);
+    } else if(n / c > 0) {
+      return process(n - n / c, a, b, c, i + 1);
+    } else {
+      return i;
+    }
+  }
+}
 
-int main() {
+int processs(int n, int a, int b, int c, int i) {
+  if (n < a || n <= 0) return i;
+  else {
+    int moda, modb, modc;
+    moda = n % a;
+    modb = n % b;
+    modc = n % c;
+    if(moda > modb && moda > modc) {
+      return process(n - n / a, a, b, c, i + 1);
+    } else if(modb > moda && modb > modc) {
+      return process(n - (n / b), a, b, c, i + 1);
+    } else if (true) {
+      return process(n - n / c, a, b, c, i + 1);
+    } else {
+      return i;
+    }
+  }
+}
+
+int f() {
   int n, a, b, c;
   scanf("%d %d %d %d", &n, &a, &b, &c);
   if (a > c) {
@@ -19,22 +57,22 @@ int main() {
     b = c;
     c = d;
   }
-  printf("%d", process(n, a, b, c, 0));
+  printf("%d", processs(n, a, b, c, 0));
   return 0;
 }
 
-int process(int n, int a, int b, int c, int i) {
-  if (n % a == 0) {
-    return process(n / a, a, b, c, i + 1);
-  } else if(n % b == 0) {
-    return process(n / b, a, b, c, i + 1);
-  } else if(n % c == 0) {
-    return process(n / c, a, b, c, i + 1);
-  } else {
-    return i;
+int main() {
+  int n, a, b, c;
+  scanf("%d %d %d %d", &n, &a, &b, &c);
+  int max = -1000000000;
+  for(int x = 0; x*a <= n; x++) {
+    for(int y = 0; x*a + y*b <= n; y++) {
+      int z = (n - (a * x) - (b * y)) / c;
+      if(a*x + b*y + c*z == n && x + y + z > max) {
+        max = x + y + z;
+      }
+    }
   }
+  printf("%d", max);
+  return 0;
 }
-
-
-
-  
