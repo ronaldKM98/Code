c385860988c6fbc8804a876511178d53622ae681
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2018-12-02 01:29:25 -0500

    Bitmask

---
+++ Programming Challenges /DataStructures/bitmask.cpp
@@ -0,0 +1,41 @@
+// note: for example usage of bitset, see ch5_06_primes.cpp
+//This file is part of the material of Competitive Programming 3 book.
+#include <cmath>
+#include <cstdio>
+#include <stack>
+using namespace std;
+
+#define isOn(S, j) (S & (1 << j)) 
+#define setBit(S, j) (S |= (1 << j))
+#define clearBit(S, j) (S &= ~(1 << j))
+#define toggleBit(S, j) (S ^= (1 << j))
+#define lowBit(S) (S & (-S))
+#define setAll(S, n) (S = (1 << n) - 1)
+
+#define modulo(S, N) ((S) & (N - 1))   // returns S % N, where N is a power of 2
+#define isPowerOfTwo(S) (!(S & (S - 1)))
+#define nearestPowerOfTwo(S) ((int)pow(2.0, (int)((log((double)S) / log(2.0)) + 0.5)))
+#define turnOffLastBit(S) ((S) & (S - 1))
+#define turnOnLastZero(S) ((S) | (S + 1))
+#define turnOffLastConsecutiveBits(S) ((S) & (S + 1))
+#define turnOnLastConsecutiveZeroes(S) ((S) | (S - 1))
+
+void printSet(int vS) {                         // in binary representation                                                                                   
+  printf("S = %2d = ", vS);
+  stack<int> st;
+  while (vS)
+    st.push(vS % 2), vS /= 2;
+  while (!st.empty())                         // to reverse the print order                                                                                   
+    printf("%d", st.top()), st.pop();
+  printf("\n");
+}
+
+
+int
+main() {
+  int S = 40;
+  printSet(modulo(S, 2));
+
+  
+  return 0;
+}
