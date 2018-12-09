3fe0dbc90707da5c7119fa7f2bd901e81c5be625
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2018-12-09 13:29:25 -0500

    JollyJumpers

--- Programming Challenges /DataStructures/UVa10038.cpp
+++ Programming Challenges /DataStructures/UVa10038.cpp
@@ -3,7 +3,9 @@
 #include <bitset>
 
 using namespace std;
-
+/*
+ * UVa10038 -- JollyJumpers
+*/
 int
 main() {
     while(true) {
@@ -15,7 +17,7 @@ main() {
                 scanf("%d", &x);
                 arr[i] = x;
             }
-            bitset<3000> found = bitset<3000>().reset();
+            bitset<3000> found = bitset<3000>().reset(); // found = false
 
             for(int i = 0; i < n - 1; i++) {
                 int a = abs(arr[i] - arr[i + 1]);
@@ -23,13 +25,11 @@ main() {
                 else found[a] = true;
             }
             bool jolly = true;
-            for(int i = 0; i < n; i++) {
-                if(arr[i] < 0) jolly = false;
-                else if(!found[arr[i]]) {
-                    //printf("found[arr[%d]] = ", i);
-                    cout << found[arr[i]] << endl;
+            for(int i = 1; i < n; i++) {
+                if(!found[i]) {
                     jolly = false;
-                }
+                    break;
+                } else continue;
             }
             if(jolly) cout << "Jolly" << endl;
             else cout << "Not jolly" << endl;
