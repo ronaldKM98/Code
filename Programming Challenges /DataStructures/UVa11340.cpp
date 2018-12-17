dc4e6be9fd54721c3efd23c60fafcf3ef5c36e5d
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2018-12-17 18:51:43 -0500

    Adhoc

--- Programming Challenges /DataStructures/UVa11340.cpp
+++ Programming Challenges /DataStructures/UVa11340.cpp
@@ -18,11 +18,14 @@ main() {
         while(K--) {
             int k, v;
             scanf("%d %d", &k, &v);
+            cout << "Read k = " << k << endl;
+            cout << "Read v = " << v << endl;
             paidChars[k] = v;
+            printf("char %d = %d\n", k, v);
         }
         cout << "The map was: " << endl;
-        for(int i = 0; i < paidChars.size(); i++) {
-            
+        for (auto it = paidChars.begin(); it != paidChars.end(); ++it) {
+            cout << it->first << " -> " << it->second << '\n';
         }
         cin >> M;
         double pay = 0.00;
