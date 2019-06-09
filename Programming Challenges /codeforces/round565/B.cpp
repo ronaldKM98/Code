5755e914ee1357ceb89c5a7a9e30b4772e178219
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2019-06-09 11:51:58 -0500

    Add round

---
+++ Programming Challenges /codeforces/round565/B.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool obtain();
+
+vector<int> v;
+
+int
+main() {
+    int n, a;
+    cin >> n;
+    while(n--) {
+        cin >> a;
+        v.push_back(a);
+    }
+    cout << obtain() << endl;
+
+    return 0;
+}
+
+bool obtain() {
+    int arr [6] = {4, 8, 15, 16, 23, 42};
+    bool isPossible = true;
+    
+    cout << "aqui" << endl;
+    for(vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+        for (int i = 0; i < 6; ++i) {
+            if (*it == arr[i]) {
+                v.erase(it);
+                break;;
+            }
+            isPossible = false;
+        }
+    }
+    return isPossible;
+}
