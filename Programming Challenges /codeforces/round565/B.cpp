cc3ee8bf5b681a588a7ce3206b99e3f5f2a84578
Ronald Cardona Martínez <ronaldKM98@users.noreply.github.com>
2019-06-09 11:52:54 -0500

    Update B.cpp

--- Programming Challenges /codeforces/round565/B.cpp
+++ Programming Challenges /codeforces/round565/B.cpp
@@ -25,7 +25,7 @@ bool obtain() {
     int arr [6] = {4, 8, 15, 16, 23, 42};
     bool isPossible = true;
     
-    cout << "aqui" << endl;
+
     for(vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
         for (int i = 0; i < 6; ++i) {
             if (*it == arr[i]) {
